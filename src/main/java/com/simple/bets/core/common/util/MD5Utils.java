37c20dcba07d585e91392ebd9f53ce416f4a11d0
wangdingfeng <wangdingfeng@live.com>
2019-03-07 14:11:23 +0800

    修改加密方式

--- src/main/java/com/simple/bets/core/common/util/MD5Utils.java
+++ src/main/java/com/simple/bets/core/common/util/MD5Utils.java
@@ -1,7 +1,9 @@
 package com.simple.bets.core.common.util;
-
-import org.apache.shiro.crypto.hash.SimpleHash;
-import org.apache.shiro.util.ByteSource;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+import java.security.SecureRandom;
 
 /**
  * @Author wangdingfeng
@@ -11,27 +13,76 @@ import org.apache.shiro.util.ByteSource;
 
 public class MD5Utils {
 
-	protected MD5Utils(){
+	/**
+	 * DES算法密钥
+	 */
+	private static final byte[] DES_KEY = { 21, 1, -110, 82, -32, -85, -128, -65 };
 
+	/**
+	 * 数据加密，算法（DES）
+	 *
+	 * @param data
+	 *            要进行加密的数据
+	 * @return 加密后的数据
+	 */
+	@SuppressWarnings("restriction")
+	public static String encryptBasedDes(String data) {
+		String encryptedData = null;
+		try {
+			// DES算法要求有一个可信任的随机数源
+			SecureRandom sr = new SecureRandom();
+			DESKeySpec deskey = new DESKeySpec(DES_KEY);
+			// 创建一个密匙工厂，然后用它把DESKeySpec转换成一个SecretKey对象
+			SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+			SecretKey key = keyFactory.generateSecret(deskey);
+			// 加密对象
+			Cipher cipher = Cipher.getInstance("DES");
+			cipher.init(Cipher.ENCRYPT_MODE, key, sr);
+			// 加密，并把字节数组编码成字符串
+			encryptedData = new sun.misc.BASE64Encoder().encode(cipher.doFinal(data.getBytes()));
+		} catch (Exception e) {
+			// log.error("加密错误，错误信息：", e);
+			throw new RuntimeException("加密错误，错误信息：", e);
+		}
+		return encryptedData;
 	}
 
-	private static final String SALT = "AdminBets";
-
-	private static final String ALGORITH_NAME = "md5";
-
-	private static final int HASH_ITERATIONS = 2;
-
-	public static String encrypt(String pswd) {
-		return new SimpleHash(ALGORITH_NAME, pswd, ByteSource.Util.bytes(SALT), HASH_ITERATIONS).toHex();
-	}
-
-	public static String encrypt(String username, String pswd) {
-		return new SimpleHash(ALGORITH_NAME, pswd, ByteSource.Util.bytes(username.toLowerCase() + SALT),
-				HASH_ITERATIONS).toHex();
+	/**
+	 * 数据解密，算法（DES）
+	 *
+	 * @param cryptData
+	 *            加密数据
+	 * @return 解密后的数据
+	 */
+	@SuppressWarnings("restriction")
+	public static String decryptBasedDes(String cryptData) {
+		String decryptedData = null;
+		try {
+			// DES算法要求有一个可信任的随机数源
+			SecureRandom sr = new SecureRandom();
+			DESKeySpec deskey = new DESKeySpec(DES_KEY);
+			// 创建一个密匙工厂，然后用它把DESKeySpec转换成一个SecretKey对象
+			SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+			SecretKey key = keyFactory.generateSecret(deskey);
+			// 解密对象
+			Cipher cipher = Cipher.getInstance("DES");
+			cipher.init(Cipher.DECRYPT_MODE, key, sr);
+			// 把字符串解码为字节数组，并解密
+			decryptedData = new String(cipher.doFinal(new sun.misc.BASE64Decoder().decodeBuffer(cryptData)));
+		} catch (Exception e) {
+			throw new RuntimeException("解密错误，错误信息：", e);
+		}
+		return decryptedData;
 	}
 
 	public static void main(String[] args) {
-		System.out.println(encrypt(SALT.toLowerCase(),"123456"));
+		String str = "tester123456";
+		// DES数据加密
+		String s1 = encryptBasedDes(str);
+		System.out.println(s1);
+		// DES数据解密
+		String s2 = decryptBasedDes(s1);
+		System.err.println(s2);
 	}
 
 }
