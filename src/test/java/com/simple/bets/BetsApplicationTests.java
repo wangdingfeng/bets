b77673914a13fb95cc79d5682dd4987a1fb6044e
wangdingfeng <wangdingfeng@live.com>
2019-03-13 10:16:54 +0800

    测试工具

--- src/test/java/com/simple/bets/BetsApplicationTests.java
+++ src/test/java/com/simple/bets/BetsApplicationTests.java
@@ -1,6 +1,5 @@
 package com.simple.bets;
 
-import cn.hutool.core.util.ReflectUtil;
 import com.simple.bets.modular.sys.model.RoleModel;
 import com.simple.bets.modular.sys.service.RoleService;
 import org.junit.Test;
@@ -8,10 +7,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import tk.mybatis.mapper.entity.Example;
-
-import java.lang.reflect.Method;
-import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -19,7 +14,7 @@ public class BetsApplicationTests {
     @Autowired
     RoleService roleService;
     @Test
-    public void contextLoads() throws Exception {
+    public void contextLoads(){
         RoleModel role = new RoleModel();
         role.setRoleName("这是一个测试角色");
         roleService.save(role);
